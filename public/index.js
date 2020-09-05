c8d310eaf9f999947a06565db058cac337fff381
Bryan Plata <brayanpbarraza@gmail.com>
2020-09-05 18:52:34 -0300

    update

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const silverbryan = [
+    {
+        frontend: ['HTML', 'CSS', 'JAVASCRIPT', 'REACT JS', 'BOOTSTRAP'],
+        backend: ['Java', 'Nodejs', 'Spring'],
+        database: ['Mysql'],
+        devops: ['AWS', 'Bash Scripting', 'heroku'],
+        misc: ['GIT', 'GNU/LINUX', 'MVC', 'SCRUM', 'UI DESING', 'SPA']
+    }
+];
